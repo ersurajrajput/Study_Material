88cf6c79b2dc89654c66064fc96d131a158fc4c2
thsurajsinghrajput <thsurajsinghrajput@gmail.com>
2024-04-23 23:33:13 +0530

    update

---
+++ languages/python/Exersise/#1_intro/ex.py
@@ -0,0 +1,23 @@
+# Q1
+# print('hey my name is suraj\nits my first exersise')   
+
+# Q2
+# print("\t python is good lang\n i like it")
+
+#Q3
+# print("                A                    ")
+# print("              A   A                 ")
+# print("            A      A                 ")
+# print("          A         A                 ")
+
+#Q4
+# print("          0000000                 ")
+# print("          0     0                 ")
+# print("          0     0                 ")
+# print("          0     0                 ")
+# print("          0000000                 ")
+
+
+
+
+
