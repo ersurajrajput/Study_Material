652675218138b1332fa21fa69301bd4493792af7
thsurajsinghrajput <thsurajsinghrajput@gmail.com>
2024-05-12 02:08:05 +0530

    books added

---
+++ languages/C/code/ch4.c
@@ -0,0 +1,9 @@
+// strcpy(s1, s2) Copies s2 into s1
+// strcat(s1, s2) Concatenates s2 onto the end of s1
+// strlen(s1) Returns the length of s1
+// strcmp(s1,
+// s2)
+// Returns 0 if s1 and s2 are the same; less than 0 if s1<s2; greater
+// than 0 if s1>s2
+// strchr(s1, ch) Returns a pointer to the first occurrence of ch in s1
+// strstr(s1, s2) Returns a pointer to the first occurrence of s2 in s1
